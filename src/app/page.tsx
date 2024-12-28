11b143d042af69cb88c233f688386184f46af0cf
kooklife <107806759+kooklife@users.noreply.github.com>
2024-12-28 13:37:43 -0500

    Add Next.js app directory structure

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import { NavigationBar } from '@/components/NavigationBar'
+import { LandingPage } from '@/components/LandingPage'
+
+export default function Home() {
+  return (
+    <main>
+      <NavigationBar />
+      <LandingPage />
+    </main>
+  )
+}
